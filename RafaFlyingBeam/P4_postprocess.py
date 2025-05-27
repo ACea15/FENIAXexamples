fa9f7bdad9a35e61040eefc293abe52d0394c3c7
Rafael Palacios <r.palacios@imperial.ac.uk>
2025-05-27 09:06:02 +0100

    add matlab routines

--- RafaFlyingBeam/P4_postprocess.py
+++ RafaFlyingBeam/P4_postprocess.py
@@ -7,7 +7,7 @@ x, y = putils.pickIntrinsic2D(sol0.data.dynamicsystem_s1.t,
                               sol0.data.dynamicsystem_s1.X1,
                               fixaxis2=dict(node=1, dim=2)) # given 2 data
 fig = uplotly.lines2d(x, y)
-fig.show()
+fig.write_image('figure1.png')
 # Postprocess:1 ends here
 
 # [[file:modelgen.org::*Symmetric velocities][Symmetric velocities:1]]
@@ -19,7 +19,7 @@ for i in range(6):
                                   fixaxis1=None,
                                   fixaxis2=dict(node=0, dim=4)) # given 2 data
     fig = uplotly.lines2d(x, y, fig)
-fig.show()
+fig.write_image('figure2.png')
 # Symmetric velocities:1 ends here
 
 # [[file:modelgen.org::*Antisymmetric velocities][Antisymmetric velocities:1]]
@@ -31,5 +31,5 @@ for i in range(6):
                                   fixaxis1=None,
                                   fixaxis2=dict(node=0, dim=4)) # given 2 data
     fig = uplotly.lines2d(x, y, fig)
-fig.show()
+fig.write_image('figure3.png')
 # Antisymmetric velocities:1 ends here
